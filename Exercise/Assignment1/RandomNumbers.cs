3dd909d4a9db9f2b462d9e77c81a2ee5a7b980be
CorruptThemAll <202206179@post.au.dk>
2022-10-14 10:25:17 +0200

    Making shit

--- Exercise/Assignment1/RandomNumbers.cs
+++ Exercise/Assignment1/RandomNumbers.cs
@@ -7,12 +7,35 @@
 
         public RandomNumbers(int count, int seed)
         {
+            if(count > 0 || count < 1000)
+            {
+                _count = count;
+                _rand = new Random(seed);
+            }
         }
 
 
         public List<int> RandomValues(int minValue, int maxValue)
         {
-            return new List<int>();
+            List<int> _list = new List<int>();
+            if (minValue < 10 || minValue > 99)
+            {
+                Console.WriteLine("Illegal minValue" + minValue);
+                return _list;
+            }
+            if (maxValue < 11 || maxValue > 100)
+            {
+                Console.WriteLine("Illegal maxvalue" + maxValue);
+                return _list;
+            }
+            
+            for(int i = 0; i < _count; i++)
+            {
+                _list.Add(_rand.Next());
+
+            }
+
+            return _list;
         }
 
         
--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Exercise.Assignment1;
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine
