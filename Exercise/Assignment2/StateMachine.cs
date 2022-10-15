46d4f91ecbcf6dce3a0987ac9a2772391addc358
CorruptThemAll <202206179@post.au.dk>
2022-10-15 18:02:56 +0200

    bedre

--- Exercise/Assignment2/StateMachine.cs
+++ Exercise/Assignment2/StateMachine.cs
@@ -2,16 +2,16 @@
 {
     public enum State
     {
-       /* Original setup
-        * Red = 1,
-        * YellowToRed = 2,
-        * YellowToGreen = 3,
-        * Green = 4
-        Måske lige meget*/
+        //Original setup
         Red = 1,
-        YellowToGreen = 2,
-        Green = 3,
-        YellowToRed = 4,
+        YellowToRed = 2,
+        YellowToGreen = 3,
+        Green = 4
+        //Måske lige meget
+        // Red = 1,
+        // YellowToGreen = 2,
+        // Green = 3,
+        // YellowToRed = 4
     }
 
     public class StateMachine
@@ -23,8 +23,9 @@
         {
             state = initialState; //siger at initialstate skal hedde state i constructor, initialstate kan man declare i program.cs
             _visiblestate = state; //Det eneste sted jeg kunne bruge de to variabler og sæt dem ligemed hinanden uden at være trapped i mit loop til at lave gentagelser.
-            
-            for (int i = 0; i < 4; i++) //Loop til der sikre at den kommer videre til den næste case.
+            Console.WriteLine(_visiblestate);
+
+            for (int i = 0; i < 9; i++) //Loop til der sikre at den kommer videre til den næste case.
             {
                 DoTransition(); //Kalder på metoden
                 //state++; //Siger at den skal tilføje 1 til enum.... Var der til at fixe mit loops logik da jeg havde visiblestate = state
@@ -39,7 +40,6 @@
             switch (_visiblestate)
             {
                 case State.Red:
-                    Console.WriteLine(_visiblestate);
                     _visiblestate = State.YellowToGreen;
                     break;
                 case State.YellowToGreen:
